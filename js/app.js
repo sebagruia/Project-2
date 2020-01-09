bbf0a520a908077e1ebf1c5525d261eb39940a6f
sebagruia <sebastiangruia@gmail.com>
2020-01-09 14:10:48 +0100

    Added responsivnes behaviour

--- js/app.js
+++ js/app.js
@@ -1,52 +1,8 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-//function shuffle(array) {
-//    var currentIndex = array.length,
-//        temporaryValue, randomIndex;
-//
-//    while (currentIndex !== 0) {
-//        randomIndex = Math.floor(Math.random() * currentIndex);
-//        currentIndex -= 1;
-//        temporaryValue = array[currentIndex];
-//        array[currentIndex] = array[randomIndex];
-//        array[randomIndex] = temporaryValue;
-//    }
-//    return array;
-//}
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 
 
 /********************************************************MEMORY GAME************************************************************/
 
-
-
-
-
-
-//'use strict';
 // *************** GLOBAL VARIABLES***************
 
 (function(){
@@ -180,6 +136,7 @@ function shuffle(array) {
 function generateRandomCards() {
 
     const deck = document.getElementById('deck-id');
+    // const listontainer = document.querySelector('.list-container');
         shuffle(allSymbols);
 
     for (let i = 0; i <= 15; i++) {
@@ -187,6 +144,7 @@ function generateRandomCards() {
         arrayCards.push(singleCard);
     }
     deck.innerHTML = arrayCards.join('');
+    // listontainer.innerHTML = arrayCards.join('');
     arrayCards = [];
     return allCards = document.querySelectorAll('.card');
 }
@@ -252,11 +210,11 @@ function timeSetting() {
         timePanel.innerHTML = timerInstance.getTimeValues();
     }
     //*******************************************************************
-    timerInstance.addEventListener('secondsUpdated', insertTimeCount);
-    timerInstance.addEventListener('targetAchieved', function () {
-        timerInstance.removeEventListener('secondsUpdated', insertTimeCount);
-        letsTryAgain();
-    });
+    // timerInstance.addEventListener('secondsUpdated', insertTimeCount);
+    // timerInstance.addEventListener('targetAchieved', function () {
+    //     timerInstance.removeEventListener('secondsUpdated', insertTimeCount);
+    //     letsTryAgain();
+    // });
     //******Setting the Stars appearance according to the Time ****
     firstThird = setTimeout(function () {
         fullStarsList[0].innerHTML = emptyStar;
